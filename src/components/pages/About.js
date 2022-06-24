a0749bb2086debc3c88e53f6f2a193c49a24cec0
Rishabh Kumar <rishabhkumar3023@gmail.com>
2022-06-24 13:01:00 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+         This is a CRUD application using react-js
+        </p>
+
+        <p className="lead">
+         This application is made by Mr. Rishabh Kumar
+        </p>
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
